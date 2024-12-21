92d0588390317966c026533e95448cedc7bab3f6
PRAKASH-KALINGRAO-AITHAL <prakash.aithal@manipal.edu>
2024-12-21 13:48:50 +0530

    Initial commit

---
+++ demo/src/main/java/com/pka/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.pka.demo.controller;
+
+import com.pka.demo.model.User;
+import com.pka.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
